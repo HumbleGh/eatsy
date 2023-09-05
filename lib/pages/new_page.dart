7e8011d8487d2d650f3960b466457e025d16dfbc
HumbleGh <isaacoseianane@gmail.com>
2023-09-05 17:24:01 +0000

    minor updates

--- lib/pages/new_page.dart
+++ lib/pages/new_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+import '../utils/app_layout.dart';
+import '../utils/app_style.dart';
 
 class NewPage extends StatefulWidget {
   const NewPage({super.key});
@@ -10,6 +14,43 @@ class NewPage extends StatefulWidget {
 class _NewPageState extends State<NewPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.blue[700],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[700],
+        elevation: 0.0,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: AppLayout.getHeight(20)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hi, Humble!',
+                      style:
+                          Styles.headLineStyle1.copyWith(color: Colors.white),
+                    ),
+                    Gap(AppLayout.getHeight(4)),
+                    Text(
+                      '23 Jan, 2021',
+                      style: TextStyle(
+                          color: Colors.blue[300],
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+                Text('Hey')
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -17,114 +17,118 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.blue[700],
-        body: ListView(
-          padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(60)),
-          children: [
-            Container(
-              padding:
-                  EdgeInsets.symmetric(horizontal: AppLayout.getHeight(20)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Hi, Humble!',
-                        style:
-                            Styles.headLineStyle1.copyWith(color: Colors.white),
-                      ),
-                      Gap(AppLayout.getHeight(4)),
-                      Text(
-                        '23 Jan, 2021',
-                        style: TextStyle(
-                            color: Colors.blue[300],
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ),
-                  Container(
-                      padding: EdgeInsets.all(AppLayout.getHeight(12)),
-                      decoration: BoxDecoration(
-                          color: Colors.blue[500],
-                          borderRadius:
-                              BorderRadius.circular(AppLayout.getHeight(8))),
-                      child: const Icon(
-                        Icons.notifications,
-                        color: Colors.white,
-                      ))
-                ],
-              ),
-            ),
-            Gap(AppLayout.getHeight(30)),
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.blue[500],
-                  borderRadius: BorderRadius.circular(12)),
-              margin: EdgeInsets.symmetric(horizontal: AppLayout.getHeight(20)),
-              padding: EdgeInsets.symmetric(
-                  horizontal: AppLayout.getHeight(20),
-                  vertical: AppLayout.getHeight(12)),
-              child: Row(
-                children: [
-                  const Icon(
-                    Icons.search,
-                    color: Colors.white70,
-                  ),
-                  Gap(AppLayout.getHeight(6)),
-                  Text(
-                    'Search',
-                    style:
-                        Styles.headLineStyle3.copyWith(color: Colors.white70),
-                  )
-                ],
-              ),
-            ),
-            Gap(AppLayout.getHeight(30)),
-            const TextMore(
-              text: 'How do you feel?',
-              isColorWhite: true,
-            ),
-            Gap(AppLayout.getHeight(30)),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FancyIconText(emoji: '😩', text: 'Sad'),
-                FancyIconText(emoji: '😒', text: 'Fine'),
-                FancyIconText(emoji: '😊', text: 'Well'),
-                FancyIconText(emoji: '😀', text: 'Excellent'),
-              ],
-            ),
-            Gap(AppLayout.getHeight(30)),
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.blue, borderRadius: BorderRadius.circular(12)),
-              padding: EdgeInsets.all(AppLayout.getHeight(8)),
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const NewPage()));
-                },
-                child: Text(
-                  'New Page',
-                  style: Styles.headLineStyle2.copyWith(color: Colors.white),
-                  textAlign: TextAlign.center,
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: Colors.blue[700],
+          body: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(
+                    horizontal: AppLayout.getHeight(20),
+                    vertical: AppLayout.getHeight(20)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Hi, Humble!',
+                          style: Styles.headLineStyle1
+                              .copyWith(color: Colors.white),
+                        ),
+                        Gap(AppLayout.getHeight(4)),
+                        Text(
+                          '23 Jan, 2021',
+                          style: TextStyle(
+                              color: Colors.blue[300],
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    Container(
+                        padding: EdgeInsets.all(AppLayout.getHeight(12)),
+                        decoration: BoxDecoration(
+                            color: Colors.blue[500],
+                            borderRadius:
+                                BorderRadius.circular(AppLayout.getHeight(8))),
+                        child: const Icon(
+                          Icons.notifications,
+                          color: Colors.white,
+                        ))
+                  ],
                 ),
               ),
-            ),
-            Expanded(
-              child: Container(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: AppLayout.getHeight(20)),
-                  decoration: const BoxDecoration(color: Colors.red),
-                  child: Container(
-                    height: 500,
-                  )),
-            ),
-          ],
-        ));
+              Gap(AppLayout.getHeight(30)),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.blue[500],
+                    borderRadius: BorderRadius.circular(12)),
+                margin:
+                    EdgeInsets.symmetric(horizontal: AppLayout.getHeight(20)),
+                padding: EdgeInsets.symmetric(
+                    horizontal: AppLayout.getHeight(20),
+                    vertical: AppLayout.getHeight(12)),
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.search,
+                      color: Colors.white70,
+                    ),
+                    Gap(AppLayout.getHeight(6)),
+                    Text(
+                      'Search',
+                      style:
+                          Styles.headLineStyle3.copyWith(color: Colors.white70),
+                    )
+                  ],
+                ),
+              ),
+              Gap(AppLayout.getHeight(30)),
+              const TextMore(
+                text: 'How do you feel?',
+                isColorWhite: true,
+              ),
+              Gap(AppLayout.getHeight(30)),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FancyIconText(emoji: '😩', text: 'Sad'),
+                  FancyIconText(emoji: '😒', text: 'Fine'),
+                  FancyIconText(emoji: '😊', text: 'Well'),
+                  FancyIconText(emoji: '😀', text: 'Excellent'),
+                ],
+              ),
+              Gap(AppLayout.getHeight(30)),
+              // Container(
+              //   decoration: BoxDecoration(
+              //       color: Colors.blue,
+              //       borderRadius: BorderRadius.circular(12)),
+              //   padding: EdgeInsets.all(AppLayout.getHeight(8)),
+              //   child: TextButton(
+              //     onPressed: () {
+              //       Navigator.push(
+              //           context,
+              //           MaterialPageRoute(
+              //               builder: (context) => const NewPage()));
+              //     },
+              //     child: Text(
+              //       'New Page',
+              //       style: Styles.headLineStyle2.copyWith(color: Colors.white),
+              //       textAlign: TextAlign.center,
+              //     ),
+              //   ),
+              // ),
+              Expanded(
+                child: Container(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: AppLayout.getHeight(20)),
+                    decoration: const BoxDecoration(color: Colors.white),
+                    child: Container()),
+              ),
+            ],
+          )),
+    );
   }
 }
